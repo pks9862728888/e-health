755a4e81030534addfc5b42607b4db4e93b793e2
DarkKnight <pks9862728888@gmail.com>
2021-05-22 15:11:10 +0530

    Initial commit

---
+++ src/main/java/com/curesio/ehealth/EHealthApplication.java
@@ -0,0 +1,13 @@
+package com.curesio.ehealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EHealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curesio/ehealth/EHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curesio.ehealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
